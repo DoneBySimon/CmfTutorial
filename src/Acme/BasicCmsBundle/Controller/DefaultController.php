d53104fbbb973f5ddccfef57ef5c68b460b514f4
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2014-09-18 22:03:23 +0200

    [Semantical Error] The annotation "@Template" in method Acme\BasicCmsBundle\Controller\DefaultController::pageAction() was never imported. Did you maybe forget to add a "use" statement for this annotation?

--- src/Acme/BasicCmsBundle/Controller/DefaultController.php
+++ src/Acme/BasicCmsBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Acme\BasicCmsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
