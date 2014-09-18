d785f1944d78e3d5a62630917ce9dc51a974f997
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2014-09-18 21:56:19 +0200

    Route Requests to a Controller

--- src/Acme/BasicCmsBundle/Controller/DefaultController.php
+++ src/Acme/BasicCmsBundle/Controller/DefaultController.php
@@ -10,4 +10,18 @@ class DefaultController extends Controller
     {
         return $this->render('AcmeBasicCmsBundle:Default:index.html.twig', array('name' => $name));
     }
+
+    /**
+     * @Template()
+     */
+    public function pageAction($contentDocument)
+    {
+        $dm = $this->get('doctrine_phpcr')->getManagerForClass('AcmeBasicCmsBundle:Post');
+        $posts = $dm->getRepository('AcmeBasicCmsBundle:Post')->findAll();
+
+        return array(
+            'page'  => $contentDocument,
+            'posts' => $posts,
+        );
+    }
 }
