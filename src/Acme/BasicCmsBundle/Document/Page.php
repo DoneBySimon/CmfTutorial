de94f975f49004be836dcb2cee772459053b5d2e
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2014-09-18 21:11:10 +0200

    The Documents

---
+++ src/Acme/BasicCmsBundle/Document/Page.php
@@ -0,0 +1,14 @@
+<?php
+namespace Acme\BasicCmsBundle\Document;
+
+use Symfony\Cmf\Component\Routing\RouteReferrersReadInterface;
+
+use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCR;
+
+/**
+ * @PHPCR\Document(referenceable=true)
+ */
+class Page implements RouteReferrersReadInterface
+{
+    use ContentTrait;
+}
