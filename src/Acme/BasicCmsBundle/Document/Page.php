79188eebe77bd17f80fad14099c4ac8dd655d50c
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2014-09-18 22:44:24 +0200

    Modify the Page Document

--- src/Acme/BasicCmsBundle/Document/Page.php
+++ src/Acme/BasicCmsBundle/Document/Page.php
@@ -1,6 +1,7 @@
 <?php
 namespace Acme\BasicCmsBundle\Document;
 
+use Knp\Menu\NodeInterface;
 use Symfony\Cmf\Component\Routing\RouteReferrersReadInterface;
 
 use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCR;
@@ -8,7 +9,36 @@ use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCR;
 /**
  * @PHPCR\Document(referenceable=true)
  */
-class Page implements RouteReferrersReadInterface
+class Page implements RouteReferrersReadInterface, NodeInterface
 {
     use ContentTrait;
+
+    /**
+     * @PHPCR\Children()
+     */
+    protected $children;
+
+    public function getName()
+    {
+        return $this->title;
+    }
+
+    public function getChildren()
+    {
+        return $this->children;
+    }
+
+    public function getOptions()
+    {
+        return array(
+            'label' => $this->title,
+            'content' => $this,
+
+            'attributes'         => array(),
+            'childrenAttributes' => array(),
+            'displayChildren'    => true,
+            'linkAttributes'     => array(),
+            'labelAttributes'    => array(),
+        );
+    }
 }
